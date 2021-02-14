d3b932695caf82a9b80437c6ac90ed18246e7b8d
Callum Walley <callumnmw@gmail.com>
2021-02-14 16:57:27 +1300

    prepull

--- src/app.py
+++ src/app.py
@@ -5,16 +5,53 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import os
+
+from dash.dependencies import Input, Output
 
 stylesheets = ['style.css']
+dataPath="data"
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+app.layout = html.Div([
+    dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-example-content')
+])
+
 app.layout = html.Div(children=[
     html.H1(children='nolum'),
-    html.h2(children='')
+
+    dcc.Dropdown(
+        options=list(map(lambda x: {'label':x, 'value':x}, os.listdir(dataPath))),
+        value='import',
+        multi=True
+    ),
+    html.Div(id='dd-output-container')
 ])
+
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('demo-dropdown', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
+
+@app.callback(Output('tabs-example-content', 'children'),
+              Input('tabs-example', 'value'))
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
