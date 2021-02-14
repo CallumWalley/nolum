29d8e5bcb55fdf90392c14bcc1750b054ede6d1c
Callum Walley <callumnmw@gmail.com>
2021-02-14 21:31:12 +1300

    stop humping my leg

--- src/app.py
+++ src/app.py
@@ -1,57 +1,99 @@
 # -*- coding: utf-8 -*-
 
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import os
+import re
+import urllib.request
 
 from dash.dependencies import Input, Output
 
-stylesheets = ['style.css']
-dataPath="data"
+# Where to look for data.
+dataPath = "data"
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
-app.layout = html.Div([
-    dcc.Tabs(id='tabs-example', value='tab-1', children=[
-        dcc.Tab(label='Tab one', value='tab-1'),
-        dcc.Tab(label='Tab two', value='tab-2'),
-    ]),
-    html.Div(id='tabs-example-content')
-])
+def get_bullshit():
+    try:
+        return [html.Q(children=re.search(r'\n<li>(.*)</li>', urllib.request.urlopen('http://cbsg.sf.net').read().decode('UTF-8')).group(1)),html.P(children=' - Noel Zeng')] 
+    except:
+        return ":("
 
 app.layout = html.Div(children=[
-    html.H1(children='nolum'),
+    html.H1(children='NoLum Cloud-Native Deep-Learning Hyper-Ledger'),
+    html.Div(id='daily-wisdom', children=get_bullshit()),
+    html.Div(id='operations', children=[
+        html.H2(children='Operations'),
+        html.Div(id='op-ingest', children=[
+            html.H3(children="Ingest new data"),
+            html.Div(children=[
+                html.Label(children="Target Account"),
+                dcc.Dropdown(
+                    id='ingest-select-account',
+                    className='account-selector',
+                    options = [{'label':'placeholder', 'value':'placeholder'}]
+                    # options=list(
+                    #     map(lambda x: {'label': x, 'value': x}, os.listdir(dataPath))),
+                )
+            ]),
+            html.Div(className='radio-scroll', children=[
+                dcc.Checklist(
+                    id='ingest-list',
+                    options=list(
+                        map(lambda x: {'label': x, 'value': x}, os.listdir(dataPath))),
+                    value=[]  # List of imported documents.
+                )
+            ]),
+            html.Button('Ingest', id='ingest-button', n_clicks=0),
+            html.Div(id='ingest-tag-table'),
+            html.Div(id='ingested-data')
 
-    dcc.Dropdown(
-        options=list(map(lambda x: {'label':x, 'value':x}, os.listdir(dataPath))),
-        value='import',
-        multi=True
-    ),
-    html.Div(id='dd-output-container')
+        ])
+    ]),
+    html.Div(id='metrics', children=[
+        html.H2(children='Metrics'),
+        dcc.Tabs(id='metrics-tab', value='tab-1', children=[
+            dcc.Tab(label='placeholder', value='tab-1'),
+            dcc.Tab(label='placeholder', value='tab-2'),
+        ]),
+        html.Div(id='metrics-tab-content')
+    ])
 ])
 
-@app.callback(
-    dash.dependencies.Output('dd-output-container', 'children'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
 
-@app.callback(Output('tabs-example-content', 'children'),
-              Input('tabs-example', 'value'))
+@ app.callback(
+    dash.dependencies.Output('ingest-tag-table', 'children'),
+    dash.dependencies.Input('ingest-button', 'n_clicks'),
+    dash.dependencies.State('ingest-list', 'value'))
+def update_output(clicks, input_files):
+    df_list= []
+    for input_file in input_files:
+        df_list.append(pd.read_csv(os.path.join(dataPath,input_file)))
+    concatted_df=pd.concat(df_list, axis=0, ignore_index=True)
+    return dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in concatted_df.columns],
+        data=concatted_df
+    )
+
+
+@app.callback(Output('metrics-tab-content', 'children'),
+              Input('metrics-tab', 'value'))
 def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
-            html.H3('Tab content 1')
+            html.H3('placeholder')
         ])
     elif tab == 'tab-2':
         return html.Div([
-            html.H3('Tab content 2')
+            html.H3('placeholder')
         ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/model.py
+++ src/model.py
@@ -1,13 +1,63 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean, JSON, Date
 
 Base = declarative_base()
+# ANZ details example
+# Type,             Details,                        Particulars,    Code,               Reference,  Amount, Date,       ForeignCurrencyAmount,      ConversionCharge
+# Visa Purchase,    4835-****-****-2031  Df,        0.64,           Google  Clou,       0.21,       -12.07, 04/12/2019, USD 7.61 converted at 0.64, This includes a currency conversion charge of $0.21
 
 class Transaction(Base):
     __tablename__:"transaction"
 
     id = Column(Integer, primary_key=True)
-    to_entity = Column(String)
-    from_entity = Column(String)
+    input_string = Column(String) # Raw unedited string.
+    input_source = Column(String) # Where come from
+    to_account = Column(String)
+    from_account = Column(String)
     amount = Column(Numeric)
-    is_credit = Column(Boolean)
+    pay_type = Column(String)
+    details = Column(String)
+    date = Column(Date)
+    tags = Column(String)
+    ml_data = Column(JSON)
+
+class InputSource(Base):
+    """A list of input sources (to avoid repeat injests)"""
+    __tablename__:"input_source"
+
+    id = Column(Integer, primary_key=True)
+    path = Column(String)
+    filename = Column(String)
+    ingest_date = Column(Datetime)
+    sha1_sum = Column(Integer)
+
+class Account(Base):
+    """List of Transactions"""
+    __tablename__:"account"
+    id = Column(Integer, primary_key=True)
+    entity = Column(String)
+
+class Entitiy(Base):
+    """Party owns Account"""
+    __tablename__:"party"
+    id = Column(Integer, primary_key=True)
+    is_you = Column(Boolean), # If true, then sheets can be injested to this persons account. 
+
+# class Invoiceable(Base):
+
+# class WorkHour(Invoiceable):
+
+# class Misc(Invoiceable):
+
+
+# class Invoice(Base):
+#     """Two entities and a list of hours"""
+#     __tablename__:"invoice"
+#     id = Column(Integer, primary_key=True)
+#     is_paid = Column(Bool)
+#     entity_paying = 
+#     entity_receiving =
+
+
+
+
