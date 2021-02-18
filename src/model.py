7e20fc9017086a8fcd0e279ae7ffe8411babcddd
Noel <hello@noel.geek.nz>
2021-02-18 20:36:59 +1300

    Everyone's in relationships

--- src/model.py
+++ src/model.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean, JSON, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, Boolean, JSON, Date, create_engine
+
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 # ANZ details example
@@ -7,13 +9,14 @@ Base = declarative_base()
 # Visa Purchase,    4835-****-****-2031  Df,        0.64,           Google  Clou,       0.21,       -12.07, 04/12/2019, USD 7.61 converted at 0.64, This includes a currency conversion charge of $0.21
 
 class Transaction(Base):
-    __tablename__:"transaction"
+    __tablename__ = "transaction"
 
     id = Column(Integer, primary_key=True)
     raw_string = Column(String) # Raw unedited string.
-    input_source = Column(String) # Where come from
-    to_account = Column(String)
-    from_account = Column(String)
+    # input_source = Column(String) # Where come from
+    input_source_id = Column(Integer,ForeignKey("input_source.id"))
+    to_account_id = Column(Integer, ForeignKey("account.id"))
+    from_account_id = Column(Integer, ForeignKey("account.id"))
     amount = Column(Numeric)
     pay_type = Column(String)
     details = Column(String)
@@ -21,27 +24,34 @@ class Transaction(Base):
     tags = Column(String)
     ml_data = Column(JSON)
 
+    # Relationships
+    input_source = relationship("InputSource")
+    to_account = relationship("Account", foreign_keys=[to_account_id])
+    from_account = relationship("Account", foreign_keys=[from_account_id])
+
 class InputSource(Base):
     """A list of input sources (to avoid repeat injests)"""
-    __tablename__:"input_source"
+    __tablename__ = "input_source"
 
     id = Column(Integer, primary_key=True)
     path = Column(String)
     filename = Column(String)
-    ingest_date = Column(Datetime)
-    sha1_sum = Column(Integer)
+    ingest_date = Column(Date)
+    sha1_sum = Column(Integer) 
 
 class Account(Base):
     """List of Transactions"""
-    __tablename__:"account"
+    __tablename__ = "account"
     id = Column(Integer, primary_key=True)
-    entity = Column(String)
+    entity_id = Column(Integer, ForeignKey("party.id"))
+    entity = relationship("Entity")
 
-class Entitiy(Base):
+class Entity(Base):
     """Party owns Account"""
-    __tablename__:"party"
+    __tablename__ = "party"
     id = Column(Integer, primary_key=True)
-    is_you = Column(Boolean), # If true, then sheets can be injested to this persons account. 
+    name = Column(String)
+    is_you = Column(Boolean) # If true, then sheets can be injested to this persons account. 
 
 # class Invoiceable(Base):
 
@@ -59,5 +69,9 @@ class Entitiy(Base):
 #     entity_receiving =
 
 
+engine = create_engine("sqlite:///data.db")
+Base.metadata.create_all(engine)
 
-
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(engine)
+session = Session()
