f132cb0fa8cad428e0812b82052536b2964bf69e
Noel <hello@noel.geek.nz>
2021-03-20 19:06:22 +1300

    Add react frontend

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from "react";
+
+function CallumQuoteBox() {
+  
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NoLum Cloud-Native Deep-Learning Hyper-Ledger, v2</h1>
+
+    </div>
+  );
+}
+
+export default App;
