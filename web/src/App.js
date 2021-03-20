fe16bf0859a6383be6ba1d53a981f0b979392228
Noel <hello@noel.geek.nz>
2021-03-20 19:39:34 +1300

    Show bs

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function CallumQuoteBox() {
-  
+  const [quote, setQuote] = useState();
+  useEffect(() => {
+    // Fetch the freshest bull.
+    fetch("/bullshit").then(
+      response => {
+        if (!response.ok){
+          throw new Error(response.statusText);
+        }
+        return response.text();
+      }
+    ).then(content => {
+      setQuote(content);
+    }).catch(error => {
+      console.error("Error occurred while fetching Callum's quotes", error);
+      setQuote(":(")
+    });
+  }, [setQuote]);
+  if (quote === undefined) {
+    return null;
+  }
+  return (<p>
+    {quote} - Callum W.
+  </p>)
 }
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>NoLum Cloud-Native Deep-Learning Hyper-Ledger, v2</h1>
+      <CallumQuoteBox />
+      <h2>Ingest new data</h2>
 
     </div>
   );
